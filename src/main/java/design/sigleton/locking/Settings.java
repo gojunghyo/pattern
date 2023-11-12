66179f044b0f08d0b361f4d45cae8c634b92f974
ITO_MZ_GOJGHO\mz_gojgho <gojgho@mz.co.kr>
2023-11-12 14:28:43 +0900

    [sigletone pattern] lazy initializer reason

--- src/main/java/design/sigleton/locking/Settings.java
+++ src/main/java/design/sigleton/locking/Settings.java
@@ -21,7 +21,17 @@ public class Settings {
      */
 
     /**
-     *  static  inner 사용 */
+     *  static  inner 사용
+     *
+     * 외부 클래스 Settings가 로딩됩니다.
+     * 내부 클래스 SettingsHolder는 이 시점에서 로딩되거나 초기화되지 않습니다.
+     * getInstance()가 처음 호출될 때, 내부 클래스의 초기화가 시작됩니다.
+     * 내부 클래스의 정적 필드 (INSTANCE)가 Settings의 새로운 인스턴스로 초기화됩니다.
+     * 자바에서 클래스 로딩과 초기화는 원자적이고 스레드 안전한 것이 보장되므로, 이 방식은 Settings의 하나의 인스턴스만 생성됨을 보장합니다.
+     * 이 메커니즘은 명시적인 동기화가 필요하지 않게 하며, 효율적이고 스레드 안전한 싱글톤 생성을 보장합니다.
+     *
+     * 요약하면, 자바의 정적 내부 클래스는 내부 클래스의 게으른 로딩과 초기화 동작을 활용하여 싱글톤 패턴을 깨끗하고 스레드 안전한 방법으로 구현하는 데 사용됩니다.
+     *  */
 
     private static class SettingsHolder {
         private static final Settings INSTANCE = new Settings();
